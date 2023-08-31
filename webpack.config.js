d3993573806792cfc0e234c39f8f5d81e26f4d6b
Meabeab <malikovigor2004@mail.ru>
2023-08-31 18:33:47 +0400

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -30,14 +30,14 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
         generator: {
-          filename: "images/[name].[hash][ext]",
+          filename: "images/[hash][ext]",
         },
       },
       {
         test: /\.(woff(2)?|eot|ttf|otf)$/i,
         type: "asset/resource",
         generator: {
-          filename: "fonts/[name].[hash][ext]",
+          filename: "fonts/[hash][ext]",
         },
       },
       {
